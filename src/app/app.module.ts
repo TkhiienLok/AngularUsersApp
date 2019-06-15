158863977513472f646672fd555f6f4942dccdc3
= <=>
2019-06-15 21:18:59 +0300

    Scratch for user app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import 'hammerjs';
+import { UsertableComponent } from './components/usertable/usertable.component';
+
+import { MatTableModule } from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
+import { UserService } from './services/user.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsertableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+	BrowserAnimationsModule,
+	MatTableModule,
+	HttpClientModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/usertable/usertable.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usertable',
+  templateUrl: './usertable.component.html',
+  styleUrls: ['./usertable.component.css']
+})
+export class UsertableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
